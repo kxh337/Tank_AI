1f98114206e3257d619ba2fa2d192b6d69978fa8
Kei Hitomi <khitomi337@gmail.com>
2014-03-15 14:47:52 -0400

    fixed mouse look
    turret now turns properly.  also deleted the third person controller
    because it probably isn’t the best way to control the tank.  Also
    adjusted the colliders properly

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -39,7 +39,7 @@ public class MouseLook : MonoBehaviour {
 			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+			transform.localEulerAngles = new Vector3(0, rotationX, rotationY);
 		}
 		else if (axes == RotationAxes.MouseX)
 		{
