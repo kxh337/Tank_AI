d5b8b33f75b3a9d9d35e416e2f52624aa37d27f7
fenru <saw106@case.edu>
2014-03-17 21:32:06 -0400

    Git is annoying

---
+++ Assets/GUI/PlayerHealthBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthBar : MonoBehaviour {
+	public int maxHealth = 100;
+	public int currentHealth = 100;
+
+	private float healthBarLength;
+
+
+	// Use this for initialization
+	void Start () {
+		healthBarLength = Screen.width - 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AdjustHealth (0);
+	}
+
+	void OnGUI () {
+		GUI.Box (new Rect (10, Screen.height - 30, healthBarLength, 20), currentHealth+"%");
+	}
+
+	// @param adj the amount of health you want to adjust from current health
+	public void AdjustHealth(int adj) {
+		currentHealth += adj;
+
+		if (currentHealth < 0) 
+						currentHealth = 0;
+		if (currentHealth > maxHealth)
+						currentHealth = maxHealth;
+		if (maxHealth < 1)
+						maxHealth = 1;
+		healthBarLength = (Screen.width - 20) * (currentHealth / (float)maxHealth);
+
+
+	}
+
+}
