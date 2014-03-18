34bd516e9a62a5199bfa851441d2d298fb9c1cd3
Hun Jae Lee <hunjaelee@icloud.com>
2014-03-18 02:05:23 -0400

    Added Floating Health Text
    Floating health text is added, but it is still visible when you are
    looking 180 degrees away from other tanks.

---
+++ Assets/GUI/FloatingHealthBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (GUIText))]
+public class FloatingHealthBar : MonoBehaviour {
+	
+	public Transform target;  // Object that this label should follow
+	public Vector3 offset = Vector3.up;    // Units in world space to offset; 1 unit above object by default
+	public float clampBorderSize = 0.05f;  // How much viewport space to leave at the borders when a label is being clamped
+	public bool useMainCamera = true;   // Use the camera tagged MainCamera
+	public Camera cameraToUse ;   // Only use this if useMainCamera is false
+	Camera cam ;
+	Transform thisTransform;
+	Transform camTransform;
+	
+	void Start () {
+		thisTransform = transform;
+		if (useMainCamera)
+			cam = Camera.main;
+		else
+			cam = cameraToUse;
+		camTransform = cam.transform;
+	}
+	
+	
+	void Update() {
+		thisTransform.position = cam.WorldToViewportPoint(target.position + offset);
+	}
+}
