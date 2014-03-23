e75eab050a350b1f0d7ca7ab89770abbf3d11cf1
Kei Hitomi <khitomi337@gmail.com>
2014-03-23 15:09:20 -0400

    Added Cannon Stuff
    Some files Steven couldn’t sync online

---
+++ Assets/Tank/CannonBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBall : MonoBehaviour {
+	public AOEScript AOE;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.CompareTag ("Player") || other.gameObject.CompareTag ("Ally") || other.gameObject.CompareTag ("Enemy")) {
+			AOE.Explode ();
+			Debug.Log("boom");
+			Destroy(this.gameObject);
+		}
+
+	}
+}
