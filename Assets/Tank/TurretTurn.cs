e7b8ac8a1a6fab01ba4d6c26323eea99557aeb7a
Kei Hitomi <khitomi337@gmail.com>
2014-03-23 14:52:13 -0400

    Tank Turns like a tank
    the tank slowly turns to the Camera’s rotation

---
+++ Assets/Tank/TurretTurn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretTurn : MonoBehaviour {
+	public GameObject cameraholder;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion amttorotate;
+		amttorotate = Quaternion.Lerp(transform.rotation,cameraholder.transform.rotation,Time.deltaTime);
+		transform.rotation = amttorotate;
+	}
+}
