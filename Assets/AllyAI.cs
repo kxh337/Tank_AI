f1037af5d0cbf9754073f7797db9c2915bab10f4
Kei Hitomi <khitomi337@gmail.com>
2014-03-23 23:05:44 -0400

    Tanks can aim but shooting isn't functional
    tanks aim using rays which are right now visible.  When the rays hits a
    tank the tank will shoot out a bullet but right now they instantiate
    where the turret is so I tried giving it an offset position but it
    didn’t work and needs playing with.  Also fixed the Enemy tank’s wheels
    so that they should move properly with the AIMovement Controller

--- Assets/AllyAI.cs
+++ Assets/AllyAI.cs
@@ -12,10 +12,19 @@ public class AllyAI : MonoBehaviour {
 	public GameObject Barrel;
 	public GameObject Turret;
 	public FloatingHealthBar healthBar;
+	public float reloadTime;
+	public float shotSpeed;
+	public Vector3 instantOffset;
 
 	public bool sighted = false;
 	public bool lowHealth = false;
 	public bool inRange = false;
+	public bool aimed = false;
+	public bool isReloaded = true;
+
+	private float canShootTime;
+	private GameObject cloneProj;
+
 
 	// Use this for initialization
 	void Start () {
@@ -29,9 +38,14 @@ public class AllyAI : MonoBehaviour {
 			Destroy(parentTank);		
 		}
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
+		Debug.DrawRay(Turret.transform.position, Turret.transform.TransformDirection(Vector3.forward)*10,Color.red);
+		if(Time.time > canShootTime){
+			isReloaded = true;
+		}
 		if (target == null)
 			sighted = false;
 		if (Health < 35) {
@@ -41,12 +55,30 @@ public class AllyAI : MonoBehaviour {
 			// wander	
 			// Enemy is seen in the SightTrigger script
 		}
+
+		//enemy sighted and not low health
 		else if (sighted && !lowHealth) {
 			Vector3 amttorotate;
 			amttorotate = Vector3.RotateTowards (Turret.transform.forward, target.transform.position - Turret.transform.position, .01f, .01f);	
-			Turret.transform.rotation = Quaternion.LookRotation (new Vector3 (amttorotate.x, 0f, amttorotate.z), new Vector3 (0f, 1f, 0f));
+			Turret.transform.rotation = Quaternion.LookRotation (amttorotate, new Vector3 (0f, 1f, 0f));
 			if (inRange){
 				// attack
+				RaycastHit hit;
+				Ray aim = new Ray(Barrel.transform.position, Turret.transform.TransformDirection(Vector3.forward));
+
+
+				Physics.Raycast(aim,out hit,10);
+				Debug.Log(hit.transform.gameObject.tag);
+				if((hit.transform.gameObject.tag == "Enemy")){
+					if(isReloaded == true){
+						Debug.Log("shot");
+						cloneProj= (GameObject)Instantiate(Projectile,Turret.transform.position+instantOffset,Turret.transform.rotation);
+						Rigidbody projRigid = cloneProj.rigidbody;
+						projRigid.AddForce(Turret.transform.forward*shotSpeed);
+						isReloaded = false;
+						canShootTime = Time.time + reloadTime;
+					}
+				}
 				if (Vector3.Distance(this.gameObject.transform.position, this.target.transform.position) > range){
 					inRange = false;
 				}
@@ -58,10 +90,12 @@ public class AllyAI : MonoBehaviour {
 				}
 			}
 		}
+
+		//enemy sighted and not low health
 		else if (lowHealth && sighted) {
 			Vector3 amttorotate;
 			amttorotate = Vector3.RotateTowards (transform.forward, - target.transform.position, .01f, .01f);	
-			transform.rotation = Quaternion.LookRotation (new Vector3 (amttorotate.x, 0f, amttorotate.z), new Vector3 (0f, 1f, 0f));
+			parentTank.transform.rotation = Quaternion.LookRotation (new Vector3 (amttorotate.x, 0f, amttorotate.z), new Vector3 (0f, 1f, 0f));
 			// flee, maybe shoot during?		
 		}
 	}
