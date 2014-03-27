7a90970aaf0fc87adfc6345232ca293ec7c48c52
kxh337 <khitomi337@gmail.com>
2014-03-27 14:29:45 -0400

    Explosions will always face the camera

---
+++ Assets/Explosion Effect/faceCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class faceCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.LookAt(Camera.main.transform.position);
+	}
+}
