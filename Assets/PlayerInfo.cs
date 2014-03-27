d153efbb47095d47d5d790bdbbc8cec768f43a01
mroseman95 <mrr77@case.edu>
2014-03-27 15:55:11 -0400

    Added Targeting Line
    Added a line renderer that shows the path the cannon will follow

--- Assets/PlayerInfo.cs
+++ Assets/PlayerInfo.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 
 public class PlayerInfo : AllyAI {
-
+	public LineRenderer predictionLine;
 	// Use this for initialization
 	void Start () {
 		audio.clip = shotSound;
 	}
-
+	
 	public void takeDamage(int damage) {
 		Health -= damage;
 		healthBar.health(-damage);
@@ -15,14 +15,14 @@ public class PlayerInfo : AllyAI {
 			GameEventManager.tankDeath(this.gameObject.tag);
 		}
 	}
-
+	
 	public void shoot(){
 		if(isPlayer && isReloaded == true){
 			if(Input.GetMouseButtonDown(0)){
 				GameObject cloneProj;
 				Debug.Log("PLayer shot");
 				cloneProj = (GameObject)Instantiate(Projectile,barrelTip.transform.position + instantOffset,Barrel.transform.rotation);
-				cloneProj.rigidbody.AddForce(Turret.transform.right*shotSpeed);
+				cloneProj.rigidbody.AddForce(Turret.transform.right*shotSpeed, ForceMode.Impulse);
 				audio.Play ();
 				isReloaded = false;
 				canShootTime = Time.time + reloadTime;
@@ -31,9 +31,30 @@ public class PlayerInfo : AllyAI {
 	}
 	// Update is called once per frame
 	void Update () {
+		UpdatePredictionLine ();
 		if(Time.time > canShootTime){
 			isReloaded = true;
 		}
 		shoot();
 	}
+	void UpdatePredictionLine() {
+		predictionLine.SetVertexCount(180);
+		for(int i = 0; i < 180; i++)
+		{
+			Vector3 posN = GetTrajectoryPoint(barrelTip.transform.position, Turret.transform.right*shotSpeed, (float)i, Physics.gravity);
+			predictionLine.SetPosition(i,posN);
+		}
+	}
+	
+	Vector3 GetTrajectoryPoint(Vector3 startingPosition, Vector3 initialVelocity, float timestep, Vector3 gravity)
+	{
+		float physicsTimestep = Time.fixedDeltaTime;
+		Vector3 stepVelocity = physicsTimestep * initialVelocity;
+		
+		//Gravity is already in meters per second, so we need meters per second per second
+		Vector3 stepGravity = physicsTimestep * physicsTimestep * gravity;
+		
+		return startingPosition + (timestep * stepVelocity) + ((( timestep * timestep + timestep) * stepGravity ) / 2.0f);
+	}
+	
 }
