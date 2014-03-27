9a1e0fdcfd39015840adada787780a1c17a09fd1
mroseman95 <mrr77@case.edu>
2014-03-27 15:58:42 -0400

    targeting comments
    added some comments to the targeting script I added

--- Assets/PlayerInfo.cs
+++ Assets/PlayerInfo.cs
@@ -22,6 +22,7 @@ public class PlayerInfo : AllyAI {
 				GameObject cloneProj;
 				Debug.Log("PLayer shot");
 				cloneProj = (GameObject)Instantiate(Projectile,barrelTip.transform.position + instantOffset,Barrel.transform.rotation);
+				//gives the cannon a short burst of force that should give it the speed, shotSpeed, in the direction of the barrel
 				cloneProj.rigidbody.AddForce(Turret.transform.right*shotSpeed, ForceMode.Impulse);
 				audio.Play ();
 				isReloaded = false;
@@ -38,20 +39,21 @@ public class PlayerInfo : AllyAI {
 		shoot();
 	}
 	void UpdatePredictionLine() {
+		//sets up the number of secttions the line renderer is split up into
 		predictionLine.SetVertexCount(180);
+		//runs through each position and finds where it should be located
 		for(int i = 0; i < 180; i++)
 		{
 			Vector3 posN = GetTrajectoryPoint(barrelTip.transform.position, Turret.transform.right*shotSpeed, (float)i, Physics.gravity);
 			predictionLine.SetPosition(i,posN);
 		}
 	}
-	
+	//formulat that runs through the physics of projectile motion step by step to find where the next section of the line should be located
 	Vector3 GetTrajectoryPoint(Vector3 startingPosition, Vector3 initialVelocity, float timestep, Vector3 gravity)
 	{
 		float physicsTimestep = Time.fixedDeltaTime;
 		Vector3 stepVelocity = physicsTimestep * initialVelocity;
-		
-		//Gravity is already in meters per second, so we need meters per second per second
+
 		Vector3 stepGravity = physicsTimestep * physicsTimestep * gravity;
 		
 		return startingPosition + (timestep * stepVelocity) + ((( timestep * timestep + timestep) * stepGravity ) / 2.0f);
